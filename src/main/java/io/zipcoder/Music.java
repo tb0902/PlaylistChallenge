4328c69700e78449e54ebf2f901d12691b8bf1c3
tb0902 <tbruton417@gmail.com>
2024-03-24 22:16:45 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,42 @@
 package io.zipcoder;
 
+
+import java.util.LinkedList;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        //looping forward
+
+        int fSkips = 0;
+        int bSkips = 0;
+
+        for (int forwards = startIndex; forwards < playList.length; forwards++) {
+
+            if (playList[forwards] != selection) {
+                fSkips++;
+            } else {
+                break;
+            }
+        }
+
+
+        //looping backward
+            for (int backwards = startIndex; backwards >= 0; backwards--) {
+
+            if (playList[backwards] != selection) {
+                bSkips++;
+
+            } else {
+                break;
+            }
+        }
+      return Math.min(fSkips,bSkips);
     }
 }
